b2ed2bb61033f312297af92541d59b840355fb74
Nono0007 <noreenmagaya24@gmail.com>
2023-03-24 13:42:42 -0700

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# install puppet-lint using Puppet
-
-package { 'puppet-lint':
+# Using Puppet, install flask from pip3
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'gem',
+  provider => 'pip3',
 }
