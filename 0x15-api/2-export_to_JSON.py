debf121cd205a6b87faa5c49d45cd407a9278039
Nono0007 <noreenmagaya24@gmail.com>
2023-04-24 17:49:36 -0700

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Returns to-do list informarmation for a given employee ID."""
+"""Returns to-do list information for a given employee ID."""
 import json
-import urllib import request
+from urllib import request
 import requests
 import sys
 if __name__ == "__main__":
