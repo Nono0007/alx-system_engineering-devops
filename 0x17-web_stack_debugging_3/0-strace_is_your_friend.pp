0f3edbdbf0901483e77f4885878f68697c32208a
Nono0007 <noreenmagaya24@gmail.com>
2023-05-09 15:37:56 -0700

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# fixes 500 error by fixing typo in wordpress
+# fixes Apache 500 error by fixing typo in wordpress
 exec { 'fix typo':
-  onlyif => 'test -e /var/www/html/wp-settings.php',
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path +> '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
