7d1a2f67c52268e339782b2eb6838c8210a89aeb
Nono0007 <noreenmagaya24@gmail.com>
2023-05-09 14:14:30 -0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """A function that queries the Reddit API and
 prints the titles of the first 10 hot posts
-listed for subreddit"""
+listed for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """prints the titles of thr first 10 hot posts listed"""
+    """prints the titles of the first 10 hot posts listed"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
-            "limit": 10
+        "limit": 10
     }
-    response = request.get(url, headers=headers, params=params,
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
